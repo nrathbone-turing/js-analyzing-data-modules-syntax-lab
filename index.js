3a560f15ec97a5baa55ed1fe4db5ac125d5dc67b
Nick Rathbone <nrathbone@joinhandshake.com>
2025-06-14 21:27:01 -0600

    Create combineUsers() function based on parameters of lab after passing provided tests

--- index.js
+++ index.js
@@ -1,4 +1,22 @@
+require('datejs'); // import `datejs` module
 
+function combineUsers(...args) {
+    // initialize return object
+    const combinedObject = {
+        users: []
+    };
+
+    // use spread operator to merge all user arrays into the `users` array
+    for (const userArray of args) {
+        combinedObject.users.push(...userArray);
+    }
+
+    // add merge_date in M/d/yyyy format
+    combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+    // return the final object
+    return combinedObject;
+}
 
 
 module.exports = {
